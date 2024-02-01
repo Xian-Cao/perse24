9990327523320904dd7196bc02304b54d9607b85
Xian-Cao <129535622+Xian-Cao@users.noreply.github.com>
2024-02-01 15:04:10 +0800

    Update t03.py

--- r0/t03.py
+++ r0/t03.py
@@ -1,1 +1,7 @@
-
+scrambled = input()
+scrambled_list = list(scrambled)
+unscrambled_list = scrambled_list[2:] + scrambled_list[:2]
+unscrambled_string = ''
+for i in unscrambled_list:
+  unscrambled_string += i
+print(unscrambled_string)
